80c8f0b9855a8ad94e9de2e44c37ecb590a1c129
briankim945 <bsk945@gmail.com>
2024-11-03 18:11:32 -0500

    Updates to vm-tlbs

--- vm-tlbs/tlb.c
+++ vm-tlbs/tlb.c
@@ -54,41 +54,25 @@ int main(int argc, char *argv[])
 
     // page size
     PAGESIZE = getpagesize();
-    // printf("Page size: %ld\n", PAGESIZE);
-    // printf("Total access operations: %ld\n", reps * NUMPAGES);
 
     // Iterations
 
     gettimeofday(&start, NULL);
-    // printf("Start time: %ld\n", starttime);
 
     jump = PAGESIZE / sizeof(int);
-    // printf("Jump size: %ld\n", jump);
     for (j = 0; j < reps; j++)
     {
-        // printf("Starting loop %ld\n", j);
         a = (int *)calloc(NUMPAGES * jump, sizeof(int));
-        // printf("Successfully allocated\n");
         for (i = 0; i < NUMPAGES * jump; i += jump)
         {
             a[i] += 1;
-            // printf("Successfully touched page %ld\n", i);
         }
-        // printf("Successfully touched all pages\n");
         free(a);
-        // printf("Completed loop %ld\n", j);
     }
-
-    // printf("Completed loops\n");
 
     gettimeofday(&end, NULL);
 
     timersub(&start, &end, &tv);
-
-    // time_dif = ((double)1000000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)) / (reps * NUMPAGES);
-
-    // printf("Total time took %ld microseconds\n", endtime - starttime);
-    // printf("It took %f microseconds per access operation on average\n", (float) tv.tv_usec / (reps * NUMPAGES));
-    printf("%ld %f\n", NUMPAGES, (float) tv.tv_usec / (reps * NUMPAGES));
+    printf("%ld %f\n", NUMPAGES, (float)tv.tv_usec / (reps * NUMPAGES));
     return 0;
 }
